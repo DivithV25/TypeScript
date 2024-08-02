e729f1afa0ff03b76d8c88ab7e61aa7fbb021e44
DivithV25 <divithv25@gmail.com>
2024-08-02 13:38:46 +0530

    defing types in different functions

---
+++ types.ts
@@ -0,0 +1,14 @@
+function display(name:string,password:string){
+return name+password;
+}
+display("divith","111")
+
+function ispaid(salary:number){
+   return true;
+}
+
+function upper(y:string){
+return y.toUpperCase();
+}
+
+

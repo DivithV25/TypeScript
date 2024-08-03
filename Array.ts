412811c13b1db06c6a35960e7845dec88ce3dfc5
DivithV25 <divithv25@gmail.com>
2024-08-03 16:15:37 +0530

    Array in typescript

---
+++ Array.ts
@@ -0,0 +1,12 @@
+const superhero: string[] =[];
+const heropower:number[]=[];
+const heropower1:Array<number>=[];
+
+type User1 = {
+    name:string
+    isActive:boolean
+
+}
+
+const allusers:User1[]=[];
+

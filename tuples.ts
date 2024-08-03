199a200261a9a4091715f2ec66c9d08116d208bf
DivithV25 <divithv25@gmail.com>
2024-08-03 17:09:44 +0530

    Tuples

---
+++ tuples.ts
@@ -0,0 +1,8 @@
+let rgb:[number,number,number]=[1,1,1]
+
+type user2 =[string,number]
+
+const newuser:user2 =["divith",12]  
+//In typescript tuples are mutable 
+newuser[0]="hc.com"
+//Type are nort well defined in typescript

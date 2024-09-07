05ba0eaa77d18b3b2af63aa3339b76ae92938d28
DivithV25 <divithv25@gmail.com>
2024-09-07 22:25:55 +0530

    classes in typescript

--- setup/src/class.ts
+++ setup/src/class.ts
@@ -1,6 +1,7 @@
 class User{
-    email : string 
-    name :string
+    public email : string 
+    private name :string
+    readonly city : string = "Jaipur"
      constructor(email :string , name : string)     {
         this.email=email;
         this.name=name
